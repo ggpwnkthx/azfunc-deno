3c0736125124459500302a7f705d899ea3c804cc
ggpwnkthx <ibjessup@gmail.com>
2025-03-07 04:17:18 +0000

    local working

--- .deno/handler.sh
+++ .deno/handler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 $SCRIPT_DIR/install.sh
-$SCRIPT_DIR/deno serve -A $SCRIPT_DIR/../func.ts
+$SCRIPT_DIR/deno serve -A --port $FUNCTIONS_CUSTOMHANDLER_PORT $SCRIPT_DIR/../func.ts
